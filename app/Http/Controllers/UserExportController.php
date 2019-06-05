0f7ffa214a92285ecc18478a966e9f25e52ef1f3
Sergio Ojeda <soj3da@gmail.com>
2019-06-05 20:42:26 -0300

    Queue Export

--- app/Http/Controllers/UserExportController.php
+++ app/Http/Controllers/UserExportController.php
@@ -4,12 +4,17 @@ namespace App\Http\Controllers;
 
 use App\Exports\UsersExport;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Maatwebsite\Excel\Facades\Excel;
 
 class UserExportController extends Controller
 {
     public function __invoke()
     {
-        return Excel::download(new UsersExport, 'users.xlsx');
+        (new UsersExport())->queue('users.xlsx');
+
+        return response('Job Creado', Response::HTTP_OK, ['Content-Type' => 'text/plain']);
+
+        // return Excel::download(new UsersExport, 'users.xlsx');
     }
 }
